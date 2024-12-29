d9e3663d669f71c6a468206d22a08e1c4cf66cec
Hamza <hamzakattan@outlook.com>
2024-12-30 02:30:24 +0300

    feat(github-pages): implement workflows for deploying gh-pages

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  basePath: "/guide",
+  output: 'export',
   images: {
     unoptimized: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.ya?ml$/,
-      use: 'yaml-loader',
-    });
-    return config;
   },
 };
 
