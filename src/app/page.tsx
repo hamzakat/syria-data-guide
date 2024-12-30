0a197f5c9e087d2d91647d5ae5e6dbd5f488bf25
Hamza <hamzakattan@outlook.com>
2024-12-30 03:34:25 +0300

    fix(github-pages): adjust the images path for gh-pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -205,7 +205,7 @@ export default  function Home() {
           {language === 'en' ? 'Congrats to Syrians!' : 'مبارك التحرير!'}
         </span>
         <Image
-            src="/flag.svg"
+            src="/nextjs-github-pages/flag.svg"
             alt="Flag"
             width={25}
             height={25}
@@ -221,7 +221,7 @@ export default  function Home() {
             </h1>
             <a href="https://github.com/hamzakat/syria-data-guide" target="_blank" rel="noopener noreferrer" className="flex items-center">
               <Image 
-                src="/github.svg"
+                src="/nextjs-github-pages/github.svg"
                 alt="GitHub Repository" 
                 width={25} 
                 height={25} 
@@ -330,7 +330,7 @@ export default  function Home() {
                   <div className="flex items-center gap-2">
                     {source.logo && (
                       <Image 
-                        src={source.logo} 
+                        src={`nextjs-github-pages/${source.logo}`} 
                         alt={`${source.title[language]} logo`} 
                         width={80} 
                         height={80} 
@@ -561,7 +561,7 @@ export default  function Home() {
 
           <a href="https://syriadata.net" target="_blank" rel="noopener noreferrer" className="flex items-center">
             <Image 
-              src="/logo-black.png"
+              src="/nextjs-github-pages/logo-black.png"
               alt="Syria Data" 
               width={140} 
               height={140} 
